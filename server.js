d4ca0dd6f8e1ff0eace2ed5f001110dffc43972c
rkrahulmishra887 <rkrahulmishra88714@gmail.com>
2020-07-17 01:19:43 +0530

    first commit

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const { request, response } = require('express');
+const express = require("express");
+const { request, response } = require("express");
 
-const path = require('path');
+const path = require("path");
 
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");
 
-const createError = require('http-errors');
+const createError = require("http-errors");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const FeedbackService = require('./services/FeedbackService');
-const SpeekersService = require('./services/SpeakerService');
+const FeedbackService = require("./services/FeedbackService");
+const SpeekersService = require("./services/SpeakerService");
 
-const feedbackService = new FeedbackService('./data/feedback.json');
-const speakersService = new SpeekersService('./data/speakers.json');
+const feedbackService = new FeedbackService("./data/feedback.json");
+const speakersService = new SpeekersService("./data/speakers.json");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 
-const port = 5000;
+const port = 8000;
 
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 app.use(
   cookieSession({
-    name: 'session',
-    keys: ['Rahul@2016', 'Rahul@2020'],
+    name: "session",
+    keys: ["Rahul@2016", "Rahul@2020"],
   })
 );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
-app.locals.siteName = 'ISA HIT Branch';
+app.locals.siteName = "ISA HIT Branch";
 
 //Middlewares are applied in Express
-app.use(express.static(path.join(__dirname, './static')));
+app.use(express.static(path.join(__dirname, "./static")));
 
 /*app.get('/throw', (request, response, next) => {
   setTimeout(() => {
@@ -61,7 +61,7 @@ app.use(async (request, response, next) => {
 });
 
 app.use(
-  '/',
+  "/",
   routes({
     feedbackService,
     speakersService,
@@ -69,7 +69,7 @@ app.use(
 );
 
 app.use((request, response, next) => {
-  return next(createError(404, 'File not found'));
+  return next(createError(404, "File not found"));
 });
 
 app.use((err, request, response, next) => {
@@ -78,7 +78,7 @@ app.use((err, request, response, next) => {
   const status = err.status || 500;
   response.locals.status = status;
   response.status(status);
-  response.render('error');
+  response.render("error");
 });
 app.listen(port, () => {
   console.log(`Express Server listening on port ${port}!`);
